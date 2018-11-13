9cfb9716548c6b732f4b1d1d4e7f5389a1db4831
Shelly Chen <shellyjychen@gmail.com>
2018-11-13 05:27:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [harry, ron, hermione, hagrid]
+  array = [harry, ron, hermione, hagrid]
 end
 
 def add_element_to_end_of_array(array, element)
